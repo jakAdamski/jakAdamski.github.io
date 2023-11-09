0eea6b15e6f097562a8644dbe9ea060211c61b95
Jakub Adamski <jakub.adamski12@gmail.com>
2023-11-09 17:29:14 +0000

    new portfolio design

--- index.js
+++ index.js
@@ -1,31 +1,35 @@
 var textChange = document.getElementById("about_me");
 var mainContent = document.querySelector("#content");
-textChange.addEventListener("mouseover" , titleChange);
-textChange.addEventListener("mouseout" , titleRevert);
+textChange.addEventListener("mouseover", titleChange);
+textChange.addEventListener("mouseout", titleRevert);
 var contact = document.getElementById("contact");
 contact.addEventListener("click", email);
 var nameSpan = document.getElementById("name");
 var surnameSpan = document.getElementById("surname");
+let goToTopButton = document.getElementById("back");
+goToTopButton.addEventListener("click", scrollToTop);
 
-function titleChange(){
-        mainContent.classList.add("content_change");
-        mainContent.innerHTML="Enthusiastic</br> Developer </br> Ready </br>To </br>Rock </br>The</br> World!";
-      }
-function titleRevert(){
-    mainContent.classList.remove("content_change");
-    mainContent.innerHTML=mainContent.innerHTML=`<h1 class="content_name">
+function titleChange() {
+  mainContent.classList.add("content_change");
+  mainContent.innerHTML = "Enthusiastic</br> Developer </br> Ready </br>To </br>Rock </br>The</br> World!";
+}
+function titleRevert() {
+  mainContent.classList.remove("content_change");
+  mainContent.innerHTML = mainContent.innerHTML = `<h1 class="content_name">
     <span id="name">Jakub</span> 
     <span id="surname">Adamski</span>
     </h1><div id="btn_div">
     <a href="/projects.html"><button id="checkout_btn">Check out!</button></a>
 </div>
     `;
-  }
-    
-function email(){
-    window.open('mailto:jakub.adamski12@gmail.com');
 }
-    
+
+function email() {
+  window.open('mailto:jakub.adamski12@gmail.com');
+}
+function scrollToTop() {
+  window.scrollTo({ top: 0, behavior: 'smooth' });
+}
 
 
 
